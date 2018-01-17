57fad97c4461f2f4ba052a51cd8af8d8c697ee2c
David Luecke <daff@neyeon.com>
2018-01-17 08:21:58 -0800

    Update to Feathers v3 (Buzzard)

--- src/feathers.js
+++ src/feathers.js
@@ -1,13 +1,11 @@
 import io from 'socket.io-client';
-import feathers from 'feathers/client';
-import hooks from 'feathers-hooks';
-import socketio from 'feathers-socketio/client';
-import authentication from 'feathers-authentication-client';
+import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio-client';
+import authentication from '@feathersjs/authentication-client';
 
 const socket = io('http://localhost:3030');
 const client = feathers();
 
-client.configure(hooks());
 client.configure(socketio(socket));
 client.configure(authentication({
   storage: window.localStorage
